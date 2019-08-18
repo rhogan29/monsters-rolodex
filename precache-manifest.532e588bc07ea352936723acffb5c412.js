e36873b032003c809c388b8709821efb3b3deaca
Robert Hogan <roberthogan@stargate.local>
2019-08-18 00:35:50 -0400

    Updates

---
+++ precache-manifest.532e588bc07ea352936723acffb5c412.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee42b46992a1f4f57d6cab3c80b0cd3f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "32e6c5c7df823de8a3b1",
+    "url": "/monsters-rolodex/static/css/main.19f65126.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/monsters-rolodex/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "32e6c5c7df823de8a3b1",
+    "url": "/monsters-rolodex/static/js/main.c3814065.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
